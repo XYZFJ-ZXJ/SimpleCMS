c5fcf5403b3fd649a7c22383d727a339c5da3254
XYZFJ-ZXJ <XYZFJ_ZXJ@163.com>
2022-02-08 12:34:36 +0800

    添加HomeController

---
+++ SimpleCMS/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleCMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        //首先用于48页的GitHub测试
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
