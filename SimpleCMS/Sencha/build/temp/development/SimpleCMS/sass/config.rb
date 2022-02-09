6021805f659e19ab2cbed67dc9b5d9de143c24aa
XYZFJ-ZXJ <XYZFJ_ZXJ@163.com>
2022-02-09 20:20:43 +0800

    添加前端部分后首次成功测试，76页

---
+++ SimpleCMS/Sencha/build/temp/development/SimpleCMS/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'E:\workspace\SimpleCMS/build/.sass-cache'
---
+++ SimpleCMS/Sencha/build/temp/production/SimpleCMS/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require E:\SimpleCMS\SimpleCMS\Sencha\app.js
